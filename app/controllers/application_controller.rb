490cbf43a92af10cd8967202c8c4ac5add4d8ffc
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-07-27 13:11:08 -0600

    update protect from forgery (#33)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   include Response
   include ExceptionHandler
+
+  protect_from_forgery with: :null_session,
+    if: Proc.new { |c| c.request.format =~ %r{application/json} }
 end
